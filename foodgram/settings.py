4a219ed2a0e4670c5d0f0005940dc2e78ed4258f
Igor Olenchuk <Olenchuk.io@yandex.ru>
2021-03-25 20:33:22 +0300

    dump.json

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -84,7 +84,12 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT', default='5432')
     }
 }
-
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
